54cd30f157f3490532f9df0bdb03e2d6227cbbfa
Juan Antonio <ja@my247coach.com>
2019-07-08 14:38:22 -0700

    next project start - Clima app

---
+++ London App Brewery projects/Classes and Objects/Classes and Objects/main.swift
@@ -0,0 +1,24 @@
+//
+//  main.swift
+//  Classes and Objects
+//
+//  Created by Saul Juan Antonio Lionheart Cuautle on 7/6/19.
+//  Copyright © 2019 Saul Juan Antonio Lionheart Cuautle. All rights reserved.
+//
+
+import Foundation
+
+print("Hello Taylor Alison Swift!")
+
+let myCar = Car()
+
+let fancyCar = Car(customerChosenColour: "Burning Red")
+
+
+let mySelfDrivingCar = SelfDrivingCar()
+
+//mySelfDrivingCar.destination = "13 Swift Way"
+
+mySelfDrivingCar.drive()
+
+
