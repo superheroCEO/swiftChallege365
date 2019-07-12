cd4c96c9dec6bc31895d78f4c357b70417879db7
Juan Antonio <ja@my247coach.com>
2019-07-11 21:10:25 -0700

    30min sess3, Ray W 1-2-3

---
+++ BullsEye/BullsEye/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  BullsEye
+//
+//  Created by Saul Juan Antonio Lionheart Cuautle on 7/11/19.
+//  Copyright © 2019 Saul Juan Antonio Lionheart Cuautle. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    @IBAction func showAlert() {
+    print("Hi Taylor Alison Swift!")
+    }
+
+}
+
