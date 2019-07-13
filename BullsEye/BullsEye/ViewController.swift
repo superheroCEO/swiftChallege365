1a316323d5ca2f48466a8da797316bc3804947dd
Juan Antonio <ja@my247coach.com>
2019-07-12 21:59:55 -0700

    30min sess3, show alert && t-Assignments review

--- BullsEye/BullsEye/ViewController.swift
+++ BullsEye/BullsEye/ViewController.swift
@@ -16,7 +16,16 @@ class ViewController: UIViewController {
     }
 
     @IBAction func showAlert() {
-    print("Hi Taylor Alison Swift!")
+    //print("Hi Taylor Alison Swift!")
+        let alert = UIAlertController(title: "Hi Taylor Alison Swift", message: "With you, I'm all in.", preferredStyle: .alert)
+        
+        let action = UIAlertAction(title: "Fantastic", style: .default, handler: nil)
+        
+        alert.addAction(action)
+        
+        present(alert, animated: true, completion: nil)
+        
+        
     }
 
 }
